8e6692645ab41135a8b68eac3d721b9e3b559364
Arun Mahendrakar <Arun_Mahendrakar@Dell.com>
2019-08-13 05:13:05 +0530

    added project

---
+++ CustomerGraph/Controllers/GraphController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerGraph.Controllers
+{
+    [Route("controller")]
+    [ApiController]
+    public class GraphController : ControllerBase
+    {
+
+    }
+}
