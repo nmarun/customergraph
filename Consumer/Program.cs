76fd42934b3b5b8c5aeb6c316ac708fca00e2a8a
Arun Mahendrakar <Arun_Mahendrakar@Dell.com>
2019-08-24 09:08:52 +0530

    added console application as a consumer

---
+++ Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using CustomerGraph.Models;
+using GraphQL.Client;
+using GraphQL.Common.Request;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var query = @"query { customers { customerNumber salesChannel } }";
+            var request = new GraphQLRequest()
+            {
+                Query = query
+            };
+
+            var graphQLClient = new GraphQLClient("http://localhost:54068/graph/");
+
+            var graphQLResponse = await graphQLClient.PostAsync(request);
+
+            string json = graphQLResponse.Data.ToString(Formatting.None);
+
+            Output output = JsonConvert.DeserializeObject<Output>(json);
+        }
+    }
+
+    public class Output
+    {
+        [JsonProperty(PropertyName = "customers")]
+        public List<Customer> Customers { get; set; }
+    }
+}
